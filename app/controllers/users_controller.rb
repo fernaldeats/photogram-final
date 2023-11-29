696c8c4e8cfae4788652f903b5be4427732aa5de
fernaldeats <42142633+fernaldeats@users.noreply.github.com>
2023-11-29 07:16:02 +0000

    got root public and signed in page running

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  skip_before_action(:authenticate_user!, { :only => [:index] })
+  
   def index
     @list_of_users = User.all.order(username: :asc)
     render(template: "users/index")
